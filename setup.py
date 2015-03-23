c808d1667eca997c10a6137fb34d841e1c3ee02e
Valerio Baldisserotto <valo@eigenlab.org>
2015-03-23 09:37:37 +0100

    Added copyright notice and license

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2015 Valerio Baldisserotto <svalo[at]libersoft[dot]it”
+
+#This file is part of firmapiu-gui.
+#
+#    firmapiu-gui is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    firmapiu-gui is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with firmapiu-gui.  If not, see <http://www.gnu.org/licenses/>.
+
 from setuptools import setup, find_packages
 changelog = 'debian/changelog'
 files=['data/icons/tango/verifica96x96.png','data/icons/tango/96px-Document-open.svg.png', 'data/icons/tango/firma96x96.png', 'data/icons/tango/impostazioni96x96.png',  'data/icons/tango/smartcardsets96x96.png', 'data/icons/tango/verifica96x96.png', 'data/icons/tango/window-close-symbolic.png','data/fpiu.svg']
